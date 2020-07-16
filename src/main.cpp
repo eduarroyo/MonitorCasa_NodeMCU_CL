1df08450457d1cfdab98b2590479f31f4385f8c9
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2020-07-16 22:07:37 +0200

    Cliente básico funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const char* ssid = "TVSANTAELLA-ARENAL";
-const char* password = "3fFxEUJb";
+const char* ssid = "*****";
+const char* password = "*****";
+const char* url = "http://monitor-actividad.herokuapp.com/actualizar";
+const char* host = "monitor-actividad.herokuapp.com";
+const char* payload = "{\"monitorId\":*****}";
+const int periodoSolicitudMilis = 15*60*1000;
 
-WiFiServer server(80);
 IPAddress ip;
 
 void setup() {
-
   Serial.begin(9600);
   delay(10);
 
@@ -33,49 +36,34 @@ void setup() {
   ip = WiFi.localIP();
   Serial.print("IP:");
   Serial.println(ip);
-
-  server.begin();
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (client) //Si hay un cliente presente
-  { 
-    Serial.println("Nuevo Cliente");
-    
-    //esperamos hasta que hayan datos disponibles
-    while(!client.available()&&client.connected())
-    {
-    delay(1);
+  HTTPClient http;
+  WiFiClient client;
+ 
+  if(WiFi.status() == WL_CONNECTED) {
+    http.begin(client, url);
+    Serial.print("[HTTP] POST... ");
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Cache-Control", "no-cache");
+    http.addHeader("Host", host);
+    int httpCode = http.POST(payload);  // Realizar petición
+
+    if (httpCode > 0) {
+      Serial.printf("Código respuesta: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();   // Obtener respuesta
+        Serial.println(payload);   // Mostrar respuesta por serial
+      }
+    } else {
+      Serial.printf("Error: %s\n", http.errorToString(httpCode).c_str());
     }
-    
-    // Leemos la primera línea de la petición del cliente.
-    String linea1 = client.readStringUntil('r');
-    Serial.println(linea1);
-    
-    client.flush(); 
-                
-    Serial.println("Enviando respuesta...");   
-    //Encabezado http    
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/html");
-    client.println("Connection: close");// La conexión se cierra después de finalizar de la respuesta
-    client.println();
-    //Pagina html  para en el navegador
-    client.println("<!DOCTYPE HTML>");
-    client.println("<html>");
-    client.println("<head><title>MonitorCasa NodeMCU CL</title>");
-    client.println("<body>");
-    client.println("<h1 align='center'>MonitorCasa NodeMCU CL</h1>");
-    client.println("<div style='text-align:center;'>");
-    client.println("<br />");
-    client.println("Sirviendo esta página desde NodeMCU.");
-    client.println("</div>");
-    client.println("</body>");
-    client.println("</html>");
-    
-    delay(1);
-    Serial.println("respuesta enviada");
-    Serial.println();
+
+    http.end();
+  } else {
+    Serial.printf("Wifi no conectada");
   }
+ 
+  delay(periodoSolicitudMilis);
 }
